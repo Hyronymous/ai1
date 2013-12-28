fb120b19d5031f4ce0e546f4f9eb3d35dde9faba
Chris Williams <littleratblue@yahoo.co.jp>
2013-12-28 01:25:29 -0800

    All ops decided. Most implemented

--- brain.d
+++ brain.d
@@ -14,14 +14,24 @@ private:
 	struct NodeDefinition {
 		BrainOp[] ops;
 	}
+	
+	enum Flag : ubyte {
+		LESS = 0x1,
+		EQUAL = 0x2,
+		ZERO = 0x4,
+		
+		CMP_MASK = 0x3
+	}
 
 	INode head;
 	NodeDefinition[] nodeTypes;
+	Memory[ void* ] memories;
 	
 	uint reg1;
 	uint reg2;
 	uint reg3;
 	
+	ubyte flags;
 	uint counter;
 	
 	final class ProcessNode : INode {
@@ -29,8 +39,10 @@ private:
 		BrainOp[] ops;
 		
 	public:
+		INode next = null;
+		
 		void process(Signal sig) {
-			foreach (op; ops) final switch (op) {
+			for (size_t pos = 0; pos < ops.length; pos++) switch (ops[pos]) {
 			case BrainOp.MORPH:
 				
 			break;
@@ -145,17 +157,63 @@ private:
 				reg3 = reg1^^reg2;
 			break;
 			
+			case BrainOp.TEST:
+				flags = 0;
+				if (reg1 < reg2) flags |= Flag.LESS;
+				if (reg1 == reg2) flags |= Flag.EQUAL;
+				if (reg3 == 0) flags |= Flag.ZERO; 
+			break;
+			
+			case BrainOp.JUMP_EQ:
+				if ((flags & Flag.EQUAL) != 0) pos += reg3;
+			break;
+			case BrainOp.JUMP_NE:
+				if ((flags & Flag.EQUAL) == 0) pos += reg3;
+			break;
+			case BrainOp.JUMP_Z:
+				if ((flags & Flag.ZERO) != 0) pos += reg3;
+			break;
+			case BrainOp.JUMP_NZ:
+				if ((flags & Flag.ZERO) == 0) pos += reg3;
+			break;
+			
+			case BrainOp.JUMP_LT:
+				if ((flags & Flag.LESS) != 0) pos += reg3;
+			break;
+			case BrainOp.JUMP_GT:
+				if ((flags & Flag.CMP_MASK) == 0) pos += reg3;
+			break;
+			case BrainOp.JUMP_LTE:
+				if ((flags & Flag.CMP_MASK) != 0) pos += reg3;
+			break;
+			case BrainOp.JUMP_GTE:
+				ubyte comp = flags & Flag.CMP_MASK;
+				if (comp == 0 || comp == Flag.EQUAL) pos += reg3;
+			break;
+			
 			case BrainOp.ESCAPE:
 				// TODO
 			break;
-			
 			case BrainOp.REMEMBER:
-				
+				Memory* mem = (&this in memories);
+				if (mem is null) {
+					mem = new Memory;
+					sig.toMemory(*mem, reg1, reg2);
+					memories[&this] = *mem;
+				}
+				else {
+					sig.fromMemory(*mem, reg1, reg2);
+					reg1 = cast(uint)mem.bData.length;
+					reg2 = cast(uint)mem.iData.length;
+				}
 			break;
-			case BrainOp.RECALL:
-				
+			
+			default:
+				reg3 = ops[pos] - BrainOp.CONST_0;
 			break;
 			}
+			
+			if (next !is null) next.process(sig);
 		}
 	}
 
--- signal.d
+++ signal.d
@@ -2,10 +2,17 @@ module signal;
 
 import exceptions;
 
+struct Memory {
+	ubyte[] bData;
+	uint[] iData;
+}
+
 class Signal {
-private:
+package:
 	size_t bPos;
 	size_t iPos;
+	size_t bStart;
+	size_t iStart;
 	ubyte[] bData;
 	uint[] iData;
 	
@@ -13,6 +20,8 @@ public:
 	void setData(ubyte[] bData, uint[] iData) {
 		bPos = 0;
 		iPos = 0;
+		bStart = bData.length;
+		iStart = iData.length;
 		this.bData = bData;
 		this.iData = iData;
 	}
@@ -45,15 +54,21 @@ public:
 		if (bData.length == 0) throw new Misfire();
 		
 		ubyte ret = bData[$-1];
-		bData.length = bData.length - 1;
+		size_t newLength = bData.length - 1;
+		bData.length = newLength;
+		if (newLength < bStart) bStart = newLength;
+		
 		return ret;
 	}
 	
-	ubyte popInt() {
-		if (bData.length == 0) throw new Misfire();
+	uint popInt() {
+		if (iData.length == 0) throw new Misfire();
 		
-		ubyte ret = bData[$-1];
-		bData.length = bData.length - 1;
+		uint ret = iData[$-1];
+		size_t newLength = iData.length - 1;
+		iData.length = newLength;
+		if (newLength < iStart) iStart = newLength;
+		
 		return ret;
 	}
 	
@@ -83,5 +98,26 @@ public:
 		else {
 			iPos--;
 		}
+	}
+	
+	void fromMemory(Memory mem, size_t bLength, size_t iLength) {
+		bData.length -= bLength;
+		if (bStart < bData.length) bStart = bData.length;
+		
+		iData.length -= iLength;
+		if (iStart < iData.length) iStart = iData.length;
+		
+		bData ~= mem.bData;
+		iData ~= mem.iData;
+	}
+	
+	void toMemory(Memory mem, size_t bLength, size_t iLength) {
+		if (
+			bLength > bData.length
+			|| iLength > iData.length
+		) throw new Misfire();
+		
+		mem.bData[] = bData[$-bLength .. $];
+		mem.iData[] = iData[$-iLength .. $];
 	}
 }
